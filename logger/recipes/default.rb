dbf708122eb5475d85a7ec171d651fb9125f6f77
Shah <shreepal@ua41f72580cec51929057.ant.amazon.com>
2013-06-18 15:43:38 -0700

    Added logging to file

--- logger/recipes/default.rb
+++ logger/recipes/default.rb
@@ -7,5 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
+require 'yaml'
+
+# create a directory in which we can create our log file
+directory "/tmp" do
+  owner "root"
+  group "root"
+  mode "755"
+  action :create
+end
+
+file = File.new("/tmp/logfile.txt","w+")
+file.puts(node.to_yaml)
+
+
 Chef::Log.info('Shreepal is testing logging.')
-Chef::Log.info(node)
+Chef::Log.info(node.to_yaml)
