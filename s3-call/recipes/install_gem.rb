1c97c688647a8b4d920f0a9d8460c4a9c6762b98
Shah <shreepal@ua41f72580cec51929057.ant.amazon.com>
2013-06-24 17:54:44 -0700

    fixed regex

--- s3-call/recipes/install_gem.rb
+++ s3-call/recipes/install_gem.rb
@@ -12,7 +12,7 @@ file_name = node['s3_call']['file_name']
 # remove the gem name from the file name (remove the version number and ".gem"
 # from the end of the name)
 gem_name = /-[\d*\.]+\.gem$/.match(file_name).pre_match
-version = /\.gem$/.match(/-[\d*\.]+\.gem$/.match(file_name).string).pre_match
+version = /[\d+\.]+/.match(/-[\d*\.]+\.gem$/.match(file_name).to_s).to_s
 Chef::Log.info("Gem name is #{gem_name} and file name is #{file_name}, version: #{version}")
 
 # installs the gem
